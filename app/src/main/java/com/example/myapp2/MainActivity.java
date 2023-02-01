302b4b46366d199b59a1bda516dfa27b6b33a836
adamjonesbis <114488278+adamjonesbis@users.noreply.github.com>
2023-02-01 17:26:10 +0000

    Test2

--- app/src/main/java/com/example/myapp2/MainActivity.java
+++ app/src/main/java/com/example/myapp2/MainActivity.java
@@ -56,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+
     private void perforLogin() {
         String email = inputEmail.getText().toString();
         String password = inputPassword.getText().toString();
