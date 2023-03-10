a7d05b2a3c908bb7623cf93f189faec58f1f2cd8
adamjonesbis <114488278+adamjonesbis@users.noreply.github.com>
2023-03-10 11:56:16 +0000

    final iteration!

--- app/src/main/java/com/example/myapp2/HACCP.java
+++ app/src/main/java/com/example/myapp2/HACCP.java
@@ -23,7 +23,6 @@ public class HACCP extends AppCompatActivity {
             public void onClick(View view) {
                 gotoUrl("https://www.fsai.ie/faqs/temperature_control.html#cooking");
             }
-
             private void gotoUrl(String s) {
                 Uri uri = Uri.parse(s);
                 startActivity(new Intent(Intent.ACTION_VIEW,uri));
